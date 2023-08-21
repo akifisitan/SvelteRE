4018a5ecec4640a7c651dbf43df75a961c93ecbe
akifisitan <102677971+akifisitan@users.noreply.github.com>
2023-08-21 21:04:30 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	daisyui: {
+		themes: ['light', 'dark', 'night']
+	},
+	plugins: [daisyui]
+};
