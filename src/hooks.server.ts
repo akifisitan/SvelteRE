34b3c611af3e84692fba74c4ce19b618bb956c5a
akifisitan <102677971+akifisitan@users.noreply.github.com>
2023-09-01 15:15:34 +0300

    Protect dashboard path completely

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,14 @@
-import type { Handle } from "@sveltejs/kit";
+import { redirect, type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
   const user = event.cookies.get("session");
   event.locals.user = user ? JSON.parse(user) : null;
+
+  if (event.url.pathname.startsWith("/dashboard")) {
+    if (!event.locals.user) {
+      throw redirect(303, "/login");
+    }
+  }
 
   return resolve(event);
 };
